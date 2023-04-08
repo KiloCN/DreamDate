70278101e11030a4623c751779f901a3de549e4a
KiloCN <kilo.cn.li@gmail.com>
2023-04-08 19:26:53 +0800

    Fix a problem of Forgot to comment out the code

--- dreamdate-autoconfig/src/main/java/cn/kilo/dreamdate_autoconfig/template/RecognizeFaceTemplate.java
+++ dreamdate-autoconfig/src/main/java/cn/kilo/dreamdate_autoconfig/template/RecognizeFaceTemplate.java
@@ -50,52 +50,52 @@ public class RecognizeFaceTemplate {
         return true;
         /**
          * End of Simulate
-         * When you configure your own alibaba face recogntion accesskey, you can delete the above code
+         * When you configure your own alibaba face recogntion accesskey, you can open the above code
          * @link dreamdate-app-server/src/main/resources/application.yml
          */
 
 
 
 
-        // "YOUR_ACCESS_KEY_ID", "YOUR_ACCESS_KEY_SECRET" 的生成请参考https://help.aliyun.com/document_detail/175144.html
-        // 如果您是用的子账号AccessKey，还需要为子账号授予权限AliyunVIAPIFullAccess，请参考https://help.aliyun.com/document_detail/145025.html
-        com.aliyun.facebody20191230.Client client = RecognizeFaceTemplate.createClient(accessKeyId, accessKeyId);
-        // 场景一，使用本地文件
-        // InputStream inputStream = new FileInputStream(new File("/tmp/recognizeFace.png"));
-        // 场景二，使用任意可访问的url
-        URL url = new URL(pic_path);
-        InputStream inputStream = url.openConnection().getInputStream();
-        com.aliyun.facebody20191230.models.RecognizeFaceAdvanceRequest recognizeFaceAdvanceRequest = new com.aliyun.facebody20191230.models.RecognizeFaceAdvanceRequest()
-                .setImageURLObject(inputStream);
-        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
-        try {
-            // 复制代码运行请自行打印 API 的返回值
-            RecognizeFaceResponse recognizeFaceResponse = client.recognizeFaceAdvance(recognizeFaceAdvanceRequest, runtime);
-            // 获取整体结果
-            String jsonString = Common.toJSONString(TeaModel.buildMap(recognizeFaceResponse));
-            System.out.println(jsonString);
-            // 获取单个字段
-            System.out.println(recognizeFaceResponse.getBody());
-
-            // check face count
-            com.alibaba.fastjson.JSONObject rootObject = JSON.parseObject(jsonString);
-            com.alibaba.fastjson.JSONObject bodyObject = rootObject.getJSONObject("body");
-            com.alibaba.fastjson.JSONObject dataObject = bodyObject.getJSONObject("Data");
-            int faceCount = dataObject.getInteger("FaceCount");
-
-            if (faceCount == 1) {
-                System.out.println("FaceCount is 1.");
-                return true;
-            } else {
-                System.out.println("FaceCount is not 1.");
-                return false;
-            }
-        } catch (TeaException teaException) {
-            // 获取整体报错信息
-            System.out.println(Common.toJSONString(teaException));
-            // 获取单个字段
-            System.out.println(teaException.getCode());
-            return false;
-        }
+//        // "YOUR_ACCESS_KEY_ID", "YOUR_ACCESS_KEY_SECRET" 的生成请参考https://help.aliyun.com/document_detail/175144.html
+//        // 如果您是用的子账号AccessKey，还需要为子账号授予权限AliyunVIAPIFullAccess，请参考https://help.aliyun.com/document_detail/145025.html
+//        com.aliyun.facebody20191230.Client client = RecognizeFaceTemplate.createClient(accessKeyId, accessKeyId);
+//        // 场景一，使用本地文件
+//        // InputStream inputStream = new FileInputStream(new File("/tmp/recognizeFace.png"));
+//        // 场景二，使用任意可访问的url
+//        URL url = new URL(pic_path);
+//        InputStream inputStream = url.openConnection().getInputStream();
+//        com.aliyun.facebody20191230.models.RecognizeFaceAdvanceRequest recognizeFaceAdvanceRequest = new com.aliyun.facebody20191230.models.RecognizeFaceAdvanceRequest()
+//                .setImageURLObject(inputStream);
+//        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+//        try {
+//            // 复制代码运行请自行打印 API 的返回值
+//            RecognizeFaceResponse recognizeFaceResponse = client.recognizeFaceAdvance(recognizeFaceAdvanceRequest, runtime);
+//            // 获取整体结果
+//            String jsonString = Common.toJSONString(TeaModel.buildMap(recognizeFaceResponse));
+//            System.out.println(jsonString);
+//            // 获取单个字段
+//            System.out.println(recognizeFaceResponse.getBody());
+//
+//            // check face count
+//            com.alibaba.fastjson.JSONObject rootObject = JSON.parseObject(jsonString);
+//            com.alibaba.fastjson.JSONObject bodyObject = rootObject.getJSONObject("body");
+//            com.alibaba.fastjson.JSONObject dataObject = bodyObject.getJSONObject("Data");
+//            int faceCount = dataObject.getInteger("FaceCount");
+//
+//            if (faceCount == 1) {
+//                System.out.println("FaceCount is 1.");
+//                return true;
+//            } else {
+//                System.out.println("FaceCount is not 1.");
+//                return false;
+//            }
+//        } catch (TeaException teaException) {
+//            // 获取整体报错信息
+//            System.out.println(Common.toJSONString(teaException));
+//            // 获取单个字段
+//            System.out.println(teaException.getCode());
+//            return false;
+//        }
     }
 }
