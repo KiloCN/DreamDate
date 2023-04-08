104be2ada3c1dc04b370a7e5f098a9106dbd9541
KiloCN <kilo.cn.li@gmail.com>
2023-04-08 16:40:41 +0800

    Implementation test of FastDFS

--- dreamdate-app-server/src/test/java/cn/kilo/dreamdate_app_server/DreamDateAppServerApplicationTests.java
+++ dreamdate-app-server/src/test/java/cn/kilo/dreamdate_app_server/DreamDateAppServerApplicationTests.java
@@ -3,15 +3,23 @@ package cn.kilo.dreamdate_app_server;
 import cn.kilo.dreamdate_app_server.service.UserService;
 import cn.kilo.dreamdate_autoconfig.sms_verification.SmsTemplate;
 import cn.kilo.dreamdate_dubbo_interface.api.UserApi;
+import com.github.tobato.fastdfs.domain.conn.FdfsWebServer;
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.Duration;
 
 @SpringBootTest(classes = DreamDateAppServerApplication.class)
+@Slf4j
 class DreamDateAppServerApplicationTests {
 
     @Autowired
@@ -23,7 +31,14 @@ class DreamDateAppServerApplicationTests {
     private SmsTemplate smsTemplate;
 
     @DubboReference
-    public UserApi userApi;
+    private UserApi userApi;
+
+    @Autowired
+    private FastFileStorageClient fastFileStorageClient;
+
+    @Autowired
+    private FdfsWebServer fdfsWebServer;
+
 
     @Test
     void contextLoads() {
@@ -48,6 +63,21 @@ class DreamDateAppServerApplicationTests {
     @Test
     void userApiTest(){
         System.out.println(userApi.queryUserByMobile("13305577548").toString());
+    }
+
+
+    @Test
+    void testFDFS() throws FileNotFoundException {
+        // 1. Specify a file
+        String filePath = "/Users/kilo.cn/Downloads/Xnip2023-04-02_15-10-26.jpg";
+        File file = new File(filePath);
+
+        // 2. Upload the file
+        StorePath storePath = fastFileStorageClient.uploadFile(new FileInputStream(file), file.length(), "jpg", null);
+
+        //3. Get the file URL
+        String url = fdfsWebServer.getWebServerUrl() + storePath.getFullPath();
+        log.info(url);
     }
 
 }
